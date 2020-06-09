66b1a6bfabcce2d9bf734b517288b28e9d4c7854
Amirkhaokhar <63934648+Amirkhaokhar@users.noreply.github.com>
2020-06-10 03:17:14 +0400

    Update arry.js

--- arry.js
+++ arry.js
@@ -1,5 +1,5 @@
 malik=["Enter your name", "Enter cell number", "Enter email"];
 for(i=0; i<malik.length; i++){
        break;
+       document.getElementById("nsm").innerText="Hello";
 }
-document.getElementById("nsm");
