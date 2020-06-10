5d229d2f648c0ccd0878e7977d4c8670aba7d5d9
Amirkhaokhar <63934648+Amirkhaokhar@users.noreply.github.com>
2020-06-10 14:50:18 +0400

    Update arry.js

--- arry.js
+++ arry.js
@@ -1,6 +1,7 @@
 malik=["Enter your name", "Enter cell number", "Enter email"];
 for(i=0; i<malik.length; i++){
-    var a= document.write([malik[2]])
+    var a= document.write([malik[2]]);
+    break;
     
     
 }
